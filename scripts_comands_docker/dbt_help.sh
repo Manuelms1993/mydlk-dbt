a5a0b8f061e90b97dfd4874c91c19feba3526e46
manuel-montero <manuelms1993@gmail.com>
2024-12-07 11:22:49 +0100

    Fix paths

--- scripts_comands_docker/dbt_help.sh
+++ scripts_comands_docker/dbt_help.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /Users/manuelmontero/MM_DLK/MyDatalake/docker
+cd "$MM_DLK_PATH/MyDatalake/docker"
 docker run --expose=10000 --rm --platform linux/x86_64 \
-   -v /Users/manuelmontero/MM_DLK/mydlk-dbt/:/usr/app/ \
-   -v /Users/manuelmontero/MM_DLK/mydlk-dbt/.dbt:/root/.dbt \
+   -v /home/manuel-montero/MM_DLK/mydlk-dbt/:/usr/app/ \
+   -v /home/manuel-montero/MM_DLK/mydlk-dbt/.dbt:/root/.dbt \
   manuelms1993/mm-dbt --help
--- scripts_comands_docker/dbt_test_conection.sh
+++ scripts_comands_docker/dbt_test_conection.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd /Users/manuelmontero/MM_DLK/MyDatalake/docker
+cd "$MM_DLK_PATH/MyDatalake/docker"
 
 docker exec -it namenode hdfs dfsadmin -safemode leave
 docker exec -it datanode hdfs dfsadmin -safemode leave
 docker run --rm --platform linux/x86_64 \
-   -v /Users/manuelmontero/MM_DLK/mydlk-dbt/:/usr/app/ \
-   -v /Users/manuelmontero/MM_DLK/mydlk-dbt/.dbt:/root/.dbt \
+   -v /home/manuel-montero/MM_DLK/mydlk-dbt/:/usr/app/ \
+   -v /home/manuel-montero/MM_DLK/mydlk-dbt/.dbt:/root/.dbt \
    manuelms1993/mm-dbt debug \
    --profiles-dir "/root/.dbt" \
    --project-dir "/usr/app/dbt/dbt_compute" \
